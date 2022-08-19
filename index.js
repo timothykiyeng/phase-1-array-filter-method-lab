1eecce01b651f11ba1fc6c958c3970c7d46572ef
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-08-19 18:45:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(array, name) {
+  const matchingDrivers = array.filter((n) =>
+    n.toLowerCase().includes(name.toLowerCase())
+  );
+  return matchingDrivers;
+}
+
+function fuzzyMatch(array, string) {
+  let length = string.length;
+  const matchNames = array.filter((n) => n.slice(0, length) === string);
+  return matchNames;
+}
+
+function matchName(array, nameInput) {
+  let matchingDriverNames = array.filter((n) => n.name === nameInput);
+  return matchingDriverNames;
+}
